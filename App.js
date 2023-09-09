9bd86a625d0439c722c3fcfa0a2350ca42d20178
KenNY <ajibskenny@gmail.com>
2023-09-09 12:45:46 +0100

    code added

---
+++ App.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { WebView } from 'react-native-webview'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+
+function HomeScreen({ navigation }) {
+
+  return (
+    <View style={[styles.container, { padding: 10 }]}>
+      <View style={styles.imageView}>
+        <View style={styles.profilePicsContainer}>
+          <Image source={require('./assets/slackProfilePicture.png')} style={styles.profilePics} />
+        </View>
+        <Text style={styles.slackName}>AjKenz</Text>
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Web')} style={styles.button}>
+        <Text style={styles.buttonText}>Open GitHub</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function WebScreen() {
+  const gitUrl = 'https://github.com/AjKenz'
+  return (
+    <View style={styles.container}>
+      <WebView source={{ uri: gitUrl }} style={styles.webViewStyle} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff0',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+  },
+  profilePicsContainer: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  profilePics: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'stretch',
+  },
+  slackName: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#000',
+    alignSelf: 'flex-start',
+    elevation: 3,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+
+  },
+  button: {
+    width: '50%',
+    height: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    marginVertical: 50,
+    borderWidth: 2,
+    elevation: 5,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  webViewStyle: {
+    width: Dimensions.get('window').width
+  }
+});
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#1F2328',
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 20
+        },
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Slack Identity' }} />
+        <Stack.Screen name="Web" component={WebScreen} options={{ title: 'Git Profile', headerBackTitleVisible: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
